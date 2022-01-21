c09c5cf261baf53aeb6e510f40da3fbcb2c6a27e
Linuxpert <g.manfre10@gmail.com>
2022-01-21 17:17:08 +0100

    added main

--- resources/views/views/main.blade.php
+++ resources/views/views/main.blade.php
@@ -1,4 +1,107 @@
 @extends('layouts.main-layout')
 @section('content')
-    <h1>prova</h1>
+    <div class="sfondo">
+        <div class="container">
+            <div class="fila">
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+
+                    <p>ACTION COMICS</p>
+
+                </div>
+
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+
+                    <p>ACTION COMICS</p>
+                </div>
+
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+                    <p>ACTION COMICS</p>
+                </div>
+
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+                    <p>ACTION COMICS</p>
+                </div>
+
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+                    <p>ACTION COMICS</p>
+                </div>
+
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+                    <p>ACTION COMICS</p>
+                </div>
+            </div>
+
+            <div class="fila">
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+                    <p>ACTION COMICS</p>
+                </div>
+
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+                    <p>ACTION COMICS</p>
+                </div>
+
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+                    <p>ACTION COMICS</p>
+                </div>
+
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+                    <p>ACTION COMICS</p>
+                </div>
+
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+                    <p>ACTION COMICS</p>
+                </div>
+
+                <div class="fumetto">
+                    <img src="https://sociologicamente.it/wp-content/uploads/2021/04/Superman.jpg" alt="">
+                    <p>ACTION COMICS</p>
+                </div>
+            </div>
+
+            <div class="btn">
+                <button>LOAD MORE</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="blueLink">
+        <div class="linkShop">
+            <div class="imgLink">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="">
+                <p>DIGITAL COMICS</p>
+            </div>
+    
+            <div class="imgLink">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="">
+                <p>DIGITAL COMICS</p>
+            </div>
+    
+            <div class="imgLink">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="">
+                <p>DIGITAL COMICS</p>
+            </div>
+    
+            <div class="imgLink">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="">
+                <p>DIGITAL COMICS</p>
+            </div>
+            
+            <div class="imgLink">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="">
+                <p>DIGITAL COMICS</p>
+            </div>
+        </div>
+        
+    </div>
 @endsection
