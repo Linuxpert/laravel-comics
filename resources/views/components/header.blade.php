237f76a2931c41b157694b153d91b5a02e4a6e98
Linuxpert <g.manfre10@gmail.com>
2022-01-21 16:10:11 +0100

    added header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,64 @@
+<header>
+
+    <div class="blueBar">
+
+    </div>
+
+    <div class="link">
+        <div class="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="">
+        </div>
+
+        <ul>
+            <li>
+                <a href="#">CHARACTERS</a>
+            </li>
+
+            <li>
+                <a href="#">COMICS</a>
+            </li>
+
+            <li>
+                <a href="#">MOVIES</a>
+            </li>
+
+            <li>
+                <a href="#">TV</a>
+            </li>
+
+            <li>
+                <a href="#">GAMES</a>
+            </li>
+
+            <li>
+                <a href="#">COLLECTIBLES</a>
+            </li>
+
+            <li>
+                <a href="#">VIDEOS</a>
+            </li>
+
+            <li>
+                <a href="#">FANS</a>
+            </li>
+
+            <li>
+                <a href="#">NEWS</a>
+            </li>
+
+            <li>
+                <a href="#">SHOP</a>
+            </li>
+        </ul>
+
+        <div class="search">
+            <input type="text" placeholder="search" class="searchInp">
+        </div>
+        
+    </div>
+
+    <div class="jumbo">
+        {{-- <img src="https://cdn.bestmovie.it/wp-content/uploads/2018/07/Teen-Titans-Go-il-film-61.jpg" alt=""> --}}
+    </div>
+
+</header>
